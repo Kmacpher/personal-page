0f3e9cea79ed28b16f1bda23a571a182a68b407b
Karen MacPherson <karen@fullstackacademy.com>
2016-10-16 22:36:59 -0400

    almost done!

---
+++ js/app.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function() {
+  $('.scrollspy').scrollSpy();
+
+  $( "#contact-form" ).submit(function( event ) {
+    event.preventDefault();
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var content = $("#content").val();
+  });
+})
+
